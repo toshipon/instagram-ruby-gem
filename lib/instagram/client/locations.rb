3f50e747b8a0b9f7098cd2be03c2922ed64326b4
Toshiaki Takahashi <toshiaki.takahashi@gengo.com>
2016-06-07 17:19:44 +0900

    Update locations.rb

--- lib/instagram/client/locations.rb
+++ lib/instagram/client/locations.rb
@@ -42,7 +42,7 @@ module Instagram
       # Returns Instagram locations within proximity of given lat,lng or foursquare venue id
       #
       # @overload location_search(options={})
-      #   @param foursquare_v2_id [String] A valid Foursquare Venue ID (v2)
+      #   @param facebook_place_id [String] A valid Facebook place ID (v2)
       #   @param lat [String] A given latitude in decimal format
       #   @param lng [String] A given longitude in decimal format
       #   @option options [Integer] :count The number of media items to retrieve.
@@ -59,8 +59,8 @@ module Instagram
         options = args.last.is_a?(Hash) ? args.pop : {}
         case args.size
         when 1
-          foursquare_v2_id = args.first
-          response = get('locations/search', options.merge(:foursquare_v2_id => foursquare_v2_id))
+          facebook_place_id = args.first
+          response = get('locations/search', options.merge(:facebook_place_id => facebook_place_id))
         when 2
           lat, lng = args
           response = get('locations/search', options.merge(:lat => lat, :lng => lng))
